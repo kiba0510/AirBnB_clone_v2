8a68867915025cfb4760b4a7c1ea182947aa0416
kiba0510 <kiba0510@gmail.com>
2021-03-28 14:58:16 -0500

    hotfix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,9 @@ class FileStorage:
         except FileNotFoundError:
             pass
 
-    def delete(self, obj=None; ):
+    def delete(self, obj=None):
         """Delete an object inside the dictionary objects"""
         if obj is not None:
             del self.__objects['{}.{}'.format(obj.__class__.__name__, obj.id)]
+        else:
+            return
