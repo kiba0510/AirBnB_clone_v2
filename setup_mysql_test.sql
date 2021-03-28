f299adfc22a4db0acc658dd28ea83fdbe861944e
kiba0510 <kiba0510@gmail.com>
2021-03-28 14:46:37 -0500

    hotfix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,7 @@
 -- Creating User Test
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-GRANT USAGE ON *.*
-    TO 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.*
-    TO 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON perfomance_schema.*
-    TO 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
+USE hbnb_test_db;
