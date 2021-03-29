bfdd64c44152fb4b999e9c31b30620f958a67a02
= <=>
2021-03-29 16:58:18 +0000

    x

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from models.city import City
+import models
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """"Class that Define the Amenities"""
-
-    __tablename__ = "amenities"
-    name = Column(String(128),
-                  nullable=False)
-
-    place_amenities = relationship("Place",
-                                   secondary='place_amenity',
-                                   viewonly=False)
+    """This class defines a Amenity by various attributes"""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
