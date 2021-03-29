a4e56d27ce207353c80d8eb0537cdb423bd3357e
= <=>
2021-03-29 16:55:12 +0000

    x

--- models/base_model.py
+++ models/base_model.py
@@ -11,6 +11,7 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60),
+                unique=True,
                 primary_key=True,
                 nullable=False)
 
@@ -39,7 +40,8 @@ class BaseModel:
                 self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -57,12 +59,12 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary.update["__class__"] = str(type(self).__name__)
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if "_sa_instance_state" in dictionary:
-            del(dictionary["_sa_instance_state"])
+
+        if '_sa_instance_state' in dictionary.keys():
+            del(dictionary['_sa_instance_state'])
 
         return (dictionary)
 
