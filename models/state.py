2835d1f5875b70ba0fcbeff26b55f5f521a42065
= <=>
2021-03-30 20:35:52 +0000

    x

--- models/state.py
+++ models/state.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""State Module for HBNB project."""
+"""This is the state class"""
+from os import getenv
+from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy.orm import relationship
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
-from os import getenv
 
 
 class State(BaseModel, Base):
-    """State class."""
-
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    else:
         @property
         def cities(self):
-            """Return list of Cities with the current state_id."""
-            lt_cities = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    lt_cities.append(city)
-            return lt_cities
+            """Getter attribute in case of file storage"""
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
