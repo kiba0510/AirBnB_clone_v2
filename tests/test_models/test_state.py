e6d075007d58c04c695db06858eb5d088d5bfaf1
= <=>
2021-03-30 21:21:48 +0000

    x

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,41 +1,69 @@
 #!/usr/bin/python3
-"""Module for test User class"""
+"""test for state"""
 import unittest
-import json
-import pep8
-import datetime
-
-from models.user import User
+import os
+from models.state import State
 from models.base_model import BaseModel
+import pep8
 
 
-class TestUser(unittest.TestCase):
-    """Test User class implementation"""
+class TestState(unittest.TestCase):
+    """this will test the State class"""
 
-    def test_doc_module(self):
-        """Module documentation"""
-        doc = User.__doc__
-        self.assertGreater(len(doc), 1)
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.state = State()
+        cls.state.name = "CA"
 
-    def test_pep8_conformance_base_model(self):
-        """Test that models/user.py conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/user.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.state
 
-    def test_pep8_conformance_test_base_model(self):
-        """Test that tests/test_models/test_user.py conforms to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        res = pep8style.check_files(['tests/test_models/test_user.py'])
-        self.assertEqual(res.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def tearDown(self):
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
-    def test_doc_constructor(self):
-        """Constructor documentation"""
-        doc = User.__init__.__doc__
-        self.assertGreater(len(doc), 1)
+    def test_pep8_Review(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/state.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_checking_for_docstring_State(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(State.__doc__)
+
+    def test_attributes_State(self):
+        """chekcing if State have attributes"""
+        self.assertTrue('id' in self.state.__dict__)
+        self.assertTrue('created_at' in self.state.__dict__)
+        self.assertTrue('updated_at' in self.state.__dict__)
+        self.assertTrue('name' in self.state.__dict__)
+
+    def test_is_subclass_State(self):
+        """test if State is subclass of BaseModel"""
+        self.assertTrue(issubclass(self.state.__class__, BaseModel), True)
+
+    def test_attribute_types_State(self):
+        """test attribute type for State"""
+        self.assertEqual(type(self.state.name), str)
+
+    @unittest.skipIf(os.environ['HBNB_TYPE_STORAGE'] == 'db',
+                     'Invalid storage mode')
+    def test_save_State(self):
+        """test if the save works"""
+        self.state.save()
+        self.assertNotEqual(self.state.created_at, self.state.updated_at)
+
+    def test_to_dict_State(self):
+        """test if dictionary works"""
+        self.assertEqual('to_dict' in dir(self.state), True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
