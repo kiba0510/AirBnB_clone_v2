6724abd1533003a69eac181952dfcef6aa5a4d4d
= <=>
2021-03-30 20:34:24 +0000

    x

--- models/city.py
+++ models/city.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""City Module for HBNB project."""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+"""This is the city class"""
+from sqlalchemy import String, DateTime, Column, ForeignKey
 from sqlalchemy.orm import relationship
-import models
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
-    """The city class, contains state ID and name."""
-
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities", cascade="delete")
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete-orphan')
