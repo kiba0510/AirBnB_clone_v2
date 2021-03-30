1dc84dc30a866792ee941b3b9b701f3a21a46ed3
= <=>
2021-03-30 20:25:11 +0000

    x!

--- models/user.py
+++ models/user.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models.place import Place
+
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This is the class for user"""
 
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128),  nullable=True)
-    last_name = Column(String(128),  nullable=True)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-    places = relationship("Place", cascade="all, delete", backref="user")
-    reviews = relationship("Review", backref="user", cascade="all, delete")
+    places = relationship("Place",
+                          backref="user",
+                          cascade='all',
+                          passive_deletes=True)
+    reviews = relationship("Review", backref="user",
+                           cascade="delete")
