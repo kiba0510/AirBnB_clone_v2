b995b464c7e5683746264da64e6a8e2ed256227e
= <=>
2021-03-30 20:51:07 +0000

    x

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models.city import City
-import models
+"""This is the amenity class"""
 from os import getenv
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    """This class defines a Amenity by various attributes"""
-
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
