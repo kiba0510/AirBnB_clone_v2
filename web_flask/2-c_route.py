66eebe96eeb5fbdb5955134d61d9cd19b848567b
kiba0510 <kiba0510@gmail.com>
2021-04-29 16:53:58 -0500

    task 2 added and 0 & 1 modified(executable)

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,5 +11,17 @@ def hello_HBNB():
     return "Hello HBNB!"
 
 
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """ HBNB!!! """
+    return "HBNB"
+
+
+@app.route('/c/<phrase>', strict_slashes=False)
+def c(phrase):
+    """ C + Phrases!!! """
+    return 'C {}'.format(phrase.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run()
