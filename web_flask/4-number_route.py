07b635e50d7de877ca026c103090707fd3f3126b
kiba0510 <kiba0510@gmail.com>
2021-05-01 11:55:43 -0500

    Task 4 added

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Starts Flask. """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """ Hello HBNB!!! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """ HBNB!!! """
+    return "HBNB"
+
+
+@app.route('/c/<phrase>', strict_slashes=False)
+def c(phrase):
+    """ C + Phrases!!! """
+    return 'C {}'.format(phrase.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<phrase>', strict_slashes=False)
+def python(phrase='is cool'):
+    """ Python + phrases!!!(is cool by default) """
+    return 'Python {}'.format(phrase.replace('_', ' '))
+
+
+@app.route('/number/<int:num>', strict_slashes=False)
+def number(num):
+    """ It's a number! """
+    if type(num) == int:
+        return ("{} is a number".format(num))
+
+
+if __name__ == '__main__':
+    app.run()
